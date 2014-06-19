64bddd1126f3e6062c3da67845ec71ac4d64841d
Rahul Behera <rahul@pristine.io>
2014-06-19 15:47:53 -0500

    build AppRTCDemo via command line update. Execute WebRTC Dance within Xcode project launch AppRTCDemo for Simulator or a real iOS devices

--- ios/build.sh
+++ ios/build.sh
@@ -1,6 +1,21 @@
-# !/bin/bash
+#!/bin/sh
 
-PROJECT_DIR="$HOME"
+#  build.sh
+#  WebRTC
+#
+#  Created by Rahul Behera on 6/18/14.
+#  Copyright (c) 2014 Pristine, Inc. All rights reserved.
+
+# Get location of the script itself .. thanks SO ! http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+PROJECT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
 WEBRTC="$PROJECT_DIR/webrtc"
 DEPOT_TOOLS="$PROJECT_DIR/depot_tools"
 BUILD="$WEBRTC/libjingle_peerconnection_builds"
@@ -13,6 +28,7 @@ function create_directory_if_not_found() {
 
 create_directory_if_not_found "$PROJECT_DIR"
 create_directory_if_not_found "$WEBRTC"
+create_directory_if_not_found "$WEBRTC/WebRTC"
 
 
 # Update/Get/Ensure the Gclient Depot Tools
@@ -62,6 +78,10 @@ function wrsim() {
     export GYP_CROSSCOMPILE=1
 }
 
+function get_revision_number() {
+    svn info $WEBRTC/trunk | awk '{ if ($1 ~ /Revision/) { print $2 } }'
+}
+
 function fetch() {
     
     DIR=`pwd`
@@ -77,56 +97,139 @@ function fetch() {
     else
         gclient sync -n --revision "src@$1"
     fi
-    
     sed -i "" '/\-framework IOKit/d' "$WEBRTC/trunk/talk/libjingle.gyp"
     cd "$WEBRTC"
     
     if [ -z $1 ] 
         then
-        gclient sync
+        sync
     else
-        gclient sync --revision "src@$1"
+        sync "$1"
     fi
  
     sed -i "" '$d' .gclient
     echo "target_os = ['ios', 'mac']" >> .gclient
     
     
-    if [ -z $1 ] 
-        then
+    if [ -z $1 ]
+    then
+        sync
+    else
+        sync "$1"
+    fi
+
+    echo "-- webrtc has been sucessfully fetched"
+
+}
+
+function sync() {
+    pull_depot_tools
+    cd $WEBRTC
+    if [ -z $1 ]
+    then
         gclient sync
     else
         gclient sync --revision "src@$1"
     fi
-
-    echo "-- webrtc has been sucessfully fetched"
-    gclient runhooks
 }
 
-
-function build_apprtc () {
-    cd "$WEBRTC/trunk"
-
+function copy_headers() {
     create_directory_if_not_found "$BUILD"
-    create_directory_if_not_found "$BUILD/headers"
-    cp -R $WEBRTC/trunk/talk/app/webrtc/objc/public/*.h $BUILD/headers
-    
-    ninja -C "out_ios/Debug-iphoneos/" AppRTCDemo
-    create_directory_if_not_found "$BUILD/Debug-iphoneos"
-    cp -R $WEBRTC/trunk/out_ios/Debug-iphoneos/*.a $BUILD/Debug-iphoneos
-
-    ninja -C "out_ios/Profile-iphoneos/" AppRTCDemo
-    create_directory_if_not_found "$BUILD/Profile-iphoneos"
-    cp -R $WEBRTC/trunk/out_ios/Profile-iphoneos/*.a $BUILD/Profile-iphoneos
-
-    ninja -C "out_ios/Release-iphoneos/" AppRTCDemo
-    create_directory_if_not_found "$BUILD/Release-iphoneos"
-    cp -R $WEBRTC/trunk/out_ios/Release-iphoneos/*.a $BUILD/Release-iphoneos
-    open "$BUILD"
+    create_directory_if_not_found "$WEBRTC/headers"
+    ln -s $WEBRTC/trunk/talk/app/webrtc/objc/public/ $WEBRTC/headers
 }
 
-function dance() {
+function build_apprtc_sim() {
+    cd "$WEBRTC/trunk"
+    wrsim
+    gclient runhooks
+
+    copy_headers
+
+
+    WEBRTC_REVISION=`get_revision_number`
+    if [ "$WEBRTC_DEBUG" = true ] ; then
+        ninja -C "out_sim/Debug-iphonesimulator/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-sim-Debug.a" $WEBRTC/trunk/out_sim/Debug-iphonesimulator/*.a
+    fi
+
+    if [ "$WEBRTC_PROFILE" = true ] ; then
+        ninja -C "out_sim/Profile-iphonesimulator/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-sim-Profile.a" $WEBRTC/trunk/out_sim/Profile-iphonesimulator/*.a
+    fi
+
+    if [ "$WEBRTC_RELEASE" = true ] ; then
+        ninja -C "out_sim/Release-iphonesimulator/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-sim-Release.a" $WEBRTC/trunk/out_sim/Release-iphonesimulator/*.a
+    fi
+}
+
+
+function build_apprtc() {
+    cd "$WEBRTC/trunk"
+    wrios
+    gclient runhooks
+
+    copy_headers
+
+    WEBRTC_REVISION=`get_revision_number`
+    if [ "$WEBRTC_DEBUG" = true ] ; then
+        ninja -C "out_ios/Debug-iphoneos/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-Debug.a" $WEBRTC/trunk/out_ios/Debug-iphoneos/*.a
+    fi
+
+    if [ "$WEBRTC_PROFILE" = true ] ; then
+        ninja -C "out_ios/Profile-iphoneos/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-Profile.a" $WEBRTC/trunk/out_ios/Profile-iphoneos/*.a
+    fi
+
+    if [ "$WEBRTC_RELEASE" = true ] ; then
+        ninja -C "out_ios/Release-iphoneos/" AppRTCDemo
+        libtool -static -o "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-Release.a" $WEBRTC/trunk/out_ios/Release-iphoneos/*.a
+    fi
+}
+
+function lipo_ia32_and_armv7() {
+    WEBRTC_REVISION=`get_revision_number`
+    if [ "$WEBRTC_DEBUG" = true ] ; then
+        lipo -create $BUILD/libWebRTC-$WEBRTC_REVISION-sim-Debug.a $BUILD/libWebRTC-$WEBRTC_REVISION-ios-Debug.a -output $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Debug.a
+        ln -s $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Debug.a $WEBRTC/libWebRTC-LATEST-Universal-Debug.a
+        echo "The libWebRTC-LATEST-Universal-Debug.a in this same directory, is revision " > $WEBRTC/libWebRTC-LATEST-Universal-Debug.a.version.txt
+        echo $WEBRTC_REVISION >> $WEBRTC/libWebRTC-LATEST-Universal-Debug.a.version.txt
+    fi
+
+    if [ "$WEBRTC_PROFILE" = true ] ; then
+        lipo -create $BUILD/libWebRTC-$WEBRTC_REVISION-sim-Profile.a $BUILD/libWebRTC-$WEBRTC_REVISION-ios-Profile.a -output $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Profile.a
+        ln -s $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Profile.a $WEBRTC/libWebRTC-LATEST-Universal-Profile.a
+        echo "The libWebRTC-LATEST-Universal-Profile.a in this same directory, is revision " > $WEBRTC/libWebRTC-LATEST-Universal-Profile.a.version.txt
+        echo $WEBRTC_REVISION >> $WEBRTC/libWebRTC-LATEST-Universal-Profile.a.version.txt
+    fi
+
+    if [ "$WEBRTC_RELEASE" = true ] ; then
+        lipo -create $BUILD/libWebRTC-$WEBRTC_REVISION-sim-Release.a $BUILD/libWebRTC-$WEBRTC_REVISION-ios-Release.a -output $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Release.a
+        ln -s $BUILD/libWebRTC-$WEBRTC_REVISION-armv7-ia32-Release.a $WEBRTC/libWebRTC-LATEST-Universal-Release.a
+        echo "The libWebRTC-LATEST-Universal-Release.a in this same directory, is revision " > $WEBRTC/libWebRTC-LATEST-Universal-Release.a.version.txt
+        echo $WEBRTC_REVISION >> $WEBRTC/libWebRTC-LATEST-Universal-Release.a.version.txt
+    fi
+
+}
+
+function get_webrtc() {
     pull_depot_tools
     fetch "$1"
-    build_apprtc
 }
+
+function build_webrtc() {
+    pull_depot_tools
+    build_apprtc
+    build_apprtc_sim
+    lipo_ia32_and_armv7
+}
+
+
+function dance() {
+    get_webrtc
+    build_webrtc
+    echo "Finished Dancing!"
+}
+
---
+++ ios/clone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#  clone.sh
+#  WebRTC
+#
+#  Created by Rahul Behera on 6/18/14.
+#  Copyright (c) 2014 Pristine, Inc. All rights reserved.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SOURCE="$(readlink "$SOURCE")"
+[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+PROJECT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source "$PROJECT_DIR/build.sh"
+pull_depot_tools
+get_webrtc
