121602e5569a6bc6eeb052179b990b6dec2d8831
Tobias Noiges <tobias@medeo.ca>
2014-07-04 12:30:45 -0700

    .bashrc tweaks for Vagrant user

--- android/provision.sh
+++ android/provision.sh
@@ -3,4 +3,5 @@ source /vagrant/build.sh
 install_dependencies
 install_jdk1_6
 
-echo "export PATH=$PATH:/vagrant/build.sh" >> /home/vagrant/.bashrc
+echo "source /vagrant/build.sh" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=$JAVA_HOME" >> /home/vagrant/.bashrc
