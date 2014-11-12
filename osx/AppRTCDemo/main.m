fb1e9c0457303f58b7b327096cd045b78a497569
Zack Manning <zack.manning@gmail.com>
2014-11-11 23:32:34 -0800

    added x64 osx build scripts

---
+++ osx/AppRTCDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppRTCDemo
+//
+//  Created by Rahul Behera on 6/18/14.
+//  Copyright (c) 2014 Pristine, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppRTCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APPRTCAppDelegate class]));
+    }
+}
