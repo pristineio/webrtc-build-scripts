5b23930fc36fd47b6783b25b16551f5c97a2edb3
Jiantao Fu <49404634@qq.com>
2016-02-20 22:37:47 -0800

    fix compile issues
    1. android vagrant vm need update outdated source.
    2. update gyp path to fix ios/mac compile issue #159

--- android/provision.sh
+++ android/provision.sh
@@ -1,5 +1,6 @@
 export VAGRANT_MACHINE=1 
 echo "export VAGRANT_MACHINE=1" >> .bashrc # we set this environment variable so that we put the webrtc code in a shared directory where the host machine can see the files and modify them
+apt-get update
 source /vagrant/build.sh
 
 install_dependencies
